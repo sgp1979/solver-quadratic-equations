7f8503d2676d5baa680c70e44b9c57056fcc85c5
sgp1979 <sgp1979@yandex.ru>
2025-04-23 22:13:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))  # default to 8080
     uvicorn.run("main:app", host="0.0.0.0", port=port)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file__), "static")), name="static")
+
 
 templates = Jinja2Templates(directory="templates")
 
